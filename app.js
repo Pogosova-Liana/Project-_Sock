92c6a08ed124e7ca399a4201ede781412b07b566
Pogosova-Liana <pogolilive@gmail.com>
2024-02-28 15:06:36 +0300

    app-config

---
+++ app.js
@@ -0,0 +1,19 @@
+require('@babel/register');
+
+const express = require('express');
+
+const indexRouter = require('./routes/index.routes');
+
+const serverConfig = require('./config/serverConfig');
+
+const app = express();
+
+const PORT = 3000;
+
+serverConfig(app)
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Serever has been started at port:${PORT}`);
+});
