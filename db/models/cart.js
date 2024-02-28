fdd8591b271e5e949a62fbba1bb49f5a650961ac
Ayur67 <aur67@mail.ru>
2024-02-28 19:52:56 +0300

    db with role done1

--- db/models/cart.js
+++ db/models/cart.js
@@ -2,16 +2,13 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Cart extends Model {
-    static associate({ User }) {
+    static associate({ User, Order_line }) {
       this.belongsTo(User, { foreignKey: 'user_id' });
+      this.hasMany(Order_line, { foreignKey: 'cart_id' });
     }
   }
   Cart.init(
-    {references: {
-      model: 'Users',
-      key: 'id',
-    },
-    onDelete: 'CASCADE',
+    {
       user_id: {
         allowNull: false,
         type: DataTypes.INTEGER,
