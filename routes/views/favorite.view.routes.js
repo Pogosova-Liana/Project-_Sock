97e057f1a509a6b75fcf0ec4f8a9a002de501658
Pogosova-Liana <pogolilive@gmail.com>
2024-02-29 17:22:12 +0300

    favorits_button fix

--- routes/views/favorite.view.routes.js
+++ routes/views/favorite.view.routes.js
@@ -5,7 +5,7 @@ const FavoritePage = require('../../components/FavoritePage');
 router.get('/', async (req, res) => {
   try {
     const { id } = res.locals.user;
-    const socks = Favorite.findAll({ where: { user_id: id }, include: Sock});
+    const socks = await Favorite.findAll({ where: { user_id: id }, include: Sock });
     res.send(res.renderComponent(FavoritePage, { title: 'Избранные товары', socks }));
   } catch ({ message }) {
     res.status(500).json({ error: message });
